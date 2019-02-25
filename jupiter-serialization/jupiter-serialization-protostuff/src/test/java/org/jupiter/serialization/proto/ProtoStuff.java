337bc3d75ff6b3b833c8ad26b1c15f23a30c14f4
msl <mslhly@163.com>
2019-02-25 19:03:19 +0800

    新增test

---
+++ jupiter-serialization/jupiter-serialization-protostuff/src/test/java/org/jupiter/serialization/proto/ProtoStuff.java
@@ -0,0 +1,18 @@
+package org.jupiter.serialization.proto;
+
+/**
+ * Created by msl on 2019/2/25.
+ */
+public class ProtoStuff {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("init");
+
+
+    }
+
+
+
+}
